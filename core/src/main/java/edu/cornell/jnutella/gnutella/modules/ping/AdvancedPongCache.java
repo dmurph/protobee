52b345e9a7c0fa9985cbbd0961fd5ea569ef9e30
Daniel Murphy <toucansam99@gmail.com>
2012-04-25 16:54:04 -0400

    better ping logic now, need to add session manager next to facilitate geting all current sessions

--- core/src/main/java/edu/cornell/jnutella/gnutella/modules/ping/AdvancedPongCache.java
+++ core/src/main/java/edu/cornell/jnutella/gnutella/modules/ping/AdvancedPongCache.java
@@ -3,98 +3,82 @@ package edu.cornell.jnutella.gnutella.modules.ping;
 import java.util.List;
 import java.util.Set;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
-import edu.cornell.jnutella.gnutella.messages.GnutellaMessage;
-import edu.cornell.jnutella.gnutella.messages.MessageHeader;
 import edu.cornell.jnutella.gnutella.messages.PongBody;
 import edu.cornell.jnutella.gnutella.modules.MaxTTL;
-import edu.cornell.jnutella.guice.SessionScope;
 import edu.cornell.jnutella.identity.NetworkIdentity;
-import edu.cornell.jnutella.util.GUID;
+import edu.cornell.jnutella.util.Clock;
 
-@SessionScope
+@Singleton
 public class AdvancedPongCache {
 
-  private final int maxPongsSent;
-  private final int maxTtl;
-  private final PingSessionModel pingModel;
-  private final NetworkIdentity identity;
-  private final AdvancedPongCacheGlobal global;
+  private final Object cacheLock = new Object();
+  private final List<CacheEntry>[] cache;
+  private final Object reserveLock = new Object();
+  private final Set<PongBody> reservePongs;
+  private final int expireTime;
+  private final Clock clock;
+  private long nextExpire;
 
+  @SuppressWarnings("unchecked")
   @Inject
-  public AdvancedPongCache(@MaxTTL int maxTtl, @MaxPongsSent int maxSent,
-      PingSessionModel pingModel, NetworkIdentity identity, AdvancedPongCacheGlobal global) {
-    this.pingModel = pingModel;
-    this.identity = identity;
-    this.maxPongsSent = maxSent;
-    this.maxTtl = maxTtl;
-    this.global = global;
+  public AdvancedPongCache(Clock clock, @MaxTTL int maxTtl, @PongExpireTime int expireTime,
+      @MaxPongsSent int maxSent) {
+    this.cache = (List<CacheEntry>[]) new List[maxTtl + 1];
+    this.reservePongs = Sets.newHashSet();
+    this.clock = clock;
+    this.expireTime = expireTime;
+    this.nextExpire = 0;
   }
 
-  public void addPong(MessageHeader header, PongBody body) {
-    if (!GUID.isModernClient(header.getGuid())) {
-      global.addToReservePongs(body);
-    }
-    synchronized (global.getCacheLock()) {
-      global.getCache()[header.getHops()].add(new CacheEntry(body, identity));
-    }
-    
-    // TODO demultiplex, need to change how session models are stored/registered
+  public Object getCacheLock() {
+    return cacheLock;
   }
 
-  public void registerPing(MessageHeader header) {
-    Preconditions.checkArgument(header.getTtl() != 2 || header.getHops() != 0,
-        "Shouldn't be registering crawler pings");
-    Preconditions.checkArgument(header.getPayloadType() == MessageHeader.F_PING);
-
-    pingModel.setAcceptGuid(header.getGuid());
-    for (int i = 1; i <= maxTtl; i++) {
-      int needed = 0;
-      if (i <= header.getTtl()) {
-        needed = maxPongsSent / header.getTtl();
-      }
-      pingModel.getNeeded()[i].set(needed);
-    }
-  }
-
-  public Iterable<GnutellaMessage> getCachedPongs(MessageHeader pingHeader) {
-    Preconditions.checkArgument(pingHeader.getPayloadType() == MessageHeader.F_PING);
-
-    List<GnutellaMessage> pongs = Lists.newArrayList();
-    
-    synchronized (global.getCacheLock()) {
-      for (int ttl = 1; ttl <= pingHeader.getTtl(); ttl++) {
-        List<CacheEntry> list = global.getCache()[ttl];
-        int needed = pingModel.getNeeded()[ttl].get();
-
-        for (int j = 0; needed > 0 && j < list.size(); j++) {
-          CacheEntry entry = list.get(j);
-          if (entry.identity == identity) {
-            continue;
-          }
-          pongs.add(new GnutellaMessage(new MessageHeader(pingHeader.getGuid(),
-              MessageHeader.F_PING_REPLY, (byte) 1, (byte) (ttl - 1)), entry.body));
-
-          needed = pingModel.getNeeded()[ttl].decrementAndGet();
-        }
-      }
-    }
-
-    return pongs;
+  public List<CacheEntry>[] getCache() {
+    return cache;
   }
 
   public boolean needsRebroadcasting() {
-    return global.needsRebroadcasting();
+    synchronized (cacheLock) {
+      long now = clock.currentTimeMillis();
+      if (now > nextExpire) {
+        moveToReserve();
+        nextExpire = now + expireTime;
+        return true;
+      }
+    }
+    return false;
   }
 
-  public Set<PongBody> getReservePongs() {
-    return global.getReserverPongs();
+  public void addToReservePongs(PongBody body) {
+    synchronized (reserveLock) {
+      reservePongs.add(body);
+    }
   }
-  
-  static class CacheEntry {
+
+  public Set<PongBody> getReserverPongs() {
+    synchronized (reserveLock) {
+      return ImmutableSet.copyOf(reservePongs);
+    }
+  }
+
+  private void moveToReserve() {
+    for (List<CacheEntry> list : cache) {
+      synchronized (reserveLock) {
+        for (CacheEntry cacheEntry : list) {
+          reservePongs.add(cacheEntry.body);
+        }
+      }
+      list.clear();
+    }
+  }
+
+  public static class CacheEntry {
     final PongBody body;
     final NetworkIdentity identity;
 
@@ -103,5 +87,4 @@ public class AdvancedPongCache {
       this.identity = identity;
     }
   }
-
 }
