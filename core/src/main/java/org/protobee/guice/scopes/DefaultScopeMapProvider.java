1a5240213e8c43d7e80092bc0a903d41e046510a
Daniel Murphy <toucansam99@gmail.com>
2012-05-02 08:04:55 -0400

    changed scopes to use keys instead of strings

--- core/src/main/java/org/protobee/guice/scopes/DefaultScopeMapProvider.java
+++ core/src/main/java/org/protobee/guice/scopes/DefaultScopeMapProvider.java
@@ -3,11 +3,12 @@ package org.protobee.guice.scopes;
 import java.util.Map;
 
 import com.google.common.collect.MapMaker;
+import com.google.inject.Key;
 import com.google.inject.Provider;
 
-public class DefaultScopeMapProvider implements Provider<Map<String, Object>> {
+public class DefaultScopeMapProvider implements Provider<Map<Key<?>, Object>> {
   @Override
-  public Map<String, Object> get() {
-    return new MapMaker().concurrencyLevel(8).makeMap();
+  public Map<Key<?>, Object> get() {
+    return new MapMaker().concurrencyLevel(8).initialCapacity(100).makeMap();
   }
 }
