704ff5229475caefbe1b1932d3c3dde962f3f9a7
Daniel Murphy <toucansam99@gmail.com>
2012-05-02 21:44:24 -0400

    added reusable filter handler to core, fixed gnutella errors with refactoring, added exclusion stuff to grapher

--- gnutella6/src/main/java/org/protobee/gnutella/GnutellaProtocolConfig.java
+++ gnutella6/src/main/java/org/protobee/gnutella/GnutellaProtocolConfig.java
@@ -3,46 +3,44 @@ package org.protobee.gnutella;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.Map;
+import java.util.Set;
 
 import org.jboss.netty.channel.ChannelHandler;
 import org.jboss.netty.handler.codec.http.HttpMessageDecoder;
 import org.jboss.netty.handler.codec.http.HttpMessageEncoder;
-import org.protobee.gnutella.session.GnutellaSessionModel;
+import org.protobee.modules.ProtocolModule;
 import org.protobee.protocol.Protocol;
 import org.protobee.protocol.ProtocolConfig;
-import org.protobee.session.SessionProtocolModules;
 
 import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
 
-
 @Singleton
 @Protocol(name = "GNUTELLA", majorVersion = 0, minorVersion = 6, headerRegex = "^GNUTELLA CONNECT/0\\.6$")
-public class GnutellaProtocolConfig implements ProtocolConfig {
+public class GnutellaProtocolConfig extends ProtocolConfig {
 
   private final Provider<ChannelHandler[]> channelsProvider;
-  private final Provider<GnutellaSessionModel> sessionModuleProvider;
   private final Provider<GnutellaHttpRequestDecoder> decoderProvider;
   private final Provider<GnutellaHttpRequestEncoder> encoderProvider;
+  private final Provider<Set<ProtocolModule>> gnutellaModules;
+  private final Set<Class<? extends ProtocolModule>> gnutellaModuleClasses;
+
   private final Protocol protocol;
 
   @Inject
   public GnutellaProtocolConfig(@Gnutella Provider<ChannelHandler[]> channelsProvider,
-      Provider<GnutellaSessionModel> sessionModuleProvider,
+      @Gnutella Provider<Set<ProtocolModule>> gnutellaModules,
+      @Gnutella Set<Class<? extends ProtocolModule>> gnutellaModuleClasses,
       Provider<GnutellaHttpRequestDecoder> decoderProvider,
       Provider<GnutellaHttpRequestEncoder> encoderProvider) {
     this.channelsProvider = channelsProvider;
-    this.sessionModuleProvider = sessionModuleProvider;
-    this.protocol = this.getClass().getAnnotation(Protocol.class);
     this.decoderProvider = decoderProvider;
     this.encoderProvider = encoderProvider;
-  }
-
-  @Override
-  public SessionProtocolModules createSessionModel() {
-    return sessionModuleProvider.get();
+    this.gnutellaModules = gnutellaModules;
+    this.gnutellaModuleClasses = gnutellaModuleClasses;
+    this.protocol = this.getClass().getAnnotation(Protocol.class);
   }
 
   @Override
@@ -56,20 +54,10 @@ public class GnutellaProtocolConfig implements ProtocolConfig {
   }
 
   @Override
-  public HttpMessageDecoder createRequestDecoder() {
-    return decoderProvider.get();
-  }
-
-  @Override
-  public HttpMessageEncoder createRequestEncoder() {
-    return encoderProvider.get();
-  }
-
-  @Override
   public Map<String, Object> getServerOptions() {
     return Maps.newHashMap();
   }
-  
+
   @Override
   public Map<String, Object> getConnectionOptions() {
     return Maps.newHashMap();
@@ -78,5 +66,25 @@ public class GnutellaProtocolConfig implements ProtocolConfig {
   @Override
   public SocketAddress getListeningAddress() {
     return new InetSocketAddress(6346);
+  }
+
+  @Override
+  public Set<ProtocolModule> createProtocolModules() {
+    return gnutellaModules.get();
+  }
+
+  @Override
+  public Set<Class<? extends ProtocolModule>> getModuleClasses() {
+    return gnutellaModuleClasses;
+  }
+
+  @Override
+  public HttpMessageDecoder createRequestDecoder() {
+    return decoderProvider.get();
+  }
+
+  @Override
+  public HttpMessageEncoder createRequestEncoder() {
+    return encoderProvider.get();
   }
 }
