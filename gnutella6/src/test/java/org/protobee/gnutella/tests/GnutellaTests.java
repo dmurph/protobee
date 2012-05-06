25052a4ab197bc61ff409db9faf8e30b59efb761
Daniel Murphy <toucansam99@gmail.com>
2012-05-06 18:49:18 -0400

    forgot gnutella test proxy

---
+++ gnutella6/src/test/java/org/protobee/gnutella/tests/GnutellaTests.java
@@ -0,0 +1,13 @@
+package org.protobee.gnutella.tests;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GnutellaTests {
+  private final static Logger log = LoggerFactory.getLogger(GnutellaTests.class);
+  @Test
+  public void test() {
+    log.info("Tests are located in the protobee-gnutella6-tests project");
+  }
+}
