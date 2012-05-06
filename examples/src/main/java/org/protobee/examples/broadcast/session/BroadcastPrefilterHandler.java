20b8a28a7d5313cd3384a4ae43cdc95028bb5531
Daniel Murphy <toucansam99@gmail.com>
2012-05-05 23:17:27 -0400

    almost finished broadcast example

---
+++ examples/src/main/java/org/protobee/examples/broadcast/session/BroadcastPrefilterHandler.java
@@ -0,0 +1,18 @@
+package org.protobee.examples.broadcast.session;
+
+import java.util.Set;
+
+import org.protobee.examples.broadcast.Broadcast;
+import org.protobee.examples.protos.BroadcasterProtos.BroadcastMessage;
+import org.protobee.protocol.Protocol;
+import org.protobee.protocol.handlers.FilterChannelHandler;
+import org.protobee.protocol.handlers.FilterMode;
+import org.protobee.stats.DropLog;
+import org.protobee.util.PreFilter;
+
+public class BroadcastPrefilterHandler extends FilterChannelHandler<BroadcastMessage> {
+  public BroadcastPrefilterHandler(@Broadcast Set<PreFilter<BroadcastMessage>> filters,
+      DropLog dropLog, Protocol protocol) {
+    super(filters, dropLog, protocol, BroadcastMessage.class, FilterMode.ERROR_ON_MISMATCHED_TYPE);
+  }
+}
