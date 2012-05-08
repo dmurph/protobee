3b3e6388f61cdf69120e7b7ccd3bb1bfe802b971
Daniel Murphy <toucansam99@gmail.com>
2012-05-08 09:21:17 -0400

    fixed some small bugs with broadcast handlers, added assembly plugin

--- examples/src/main/java/org/protobee/examples/broadcast/BroadcastMessageWriter.java
+++ examples/src/main/java/org/protobee/examples/broadcast/BroadcastMessageWriter.java
@@ -2,6 +2,7 @@ package org.protobee.examples.broadcast;
 
 import java.util.Set;
 
+import org.protobee.annotation.InjectLogger;
 import org.protobee.examples.protos.BroadcasterProtos.BroadcastMessage;
 import org.protobee.identity.NetworkIdentityManager;
 import org.protobee.network.ProtobeeMessageWriter;
@@ -9,6 +10,7 @@ import org.protobee.network.ProtobeeMessageWriter.HandshakeOptions;
 import org.protobee.protocol.ProtocolModel;
 import org.protobee.session.SessionManager;
 import org.protobee.session.SessionModel;
+import org.slf4j.Logger;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -16,6 +18,8 @@ import com.google.inject.Singleton;
 @Singleton
 public class BroadcastMessageWriter {
 
+  @InjectLogger
+  private Logger log;
   private final SessionManager sessionManager;
   private final NetworkIdentityManager identityManager;
   private final ProtocolModel broadcast;
@@ -40,6 +44,7 @@ public class BroadcastMessageWriter {
         if (sessionModel.getIdentity() == identityManager.getMe()) {
           continue;
         }
+        log.info("Sending message: " + message.clone().buildPartial());
         try {
           sessionModel.getIdentity().enterScope();
           writer.write(message, HandshakeOptions.WAIT_FOR_HANDSHAKE);
